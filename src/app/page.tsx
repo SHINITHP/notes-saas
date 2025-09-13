ca6a449ea6b3db207164915a82819efb95fc0c39
shinithp <shinithp78@gmail.com>
2025-09-13 11:20:54 +0530

    Initial Next.js setup with TypeScript and Tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <div className="bg-black">
+        <h1 className="text-red-500">hi bro</h1>
+    </div>
+  );
+}
